6f91af7aec0df3218f774c7ab3364a576ba2ae6b
Sebi0506 <stausend2002@gmail.com>
2025-03-04 21:31:07 +0100

    Löschen wird gespeichert; Deaktivieren auch

--- src/main/java/org/rapla/plugin/availability/AdminMenuEntry/AdminMenuEntryResources.java
+++ src/main/java/org/rapla/plugin/availability/AdminMenuEntry/AdminMenuEntryResources.java
@@ -5,28 +5,22 @@ import org.rapla.components.i18n.AbstractBundle;
 import org.rapla.components.i18n.BundleManager;
 import org.rapla.components.i18n.I18nBundle;
 import org.rapla.inject.Extension;
-
 import javax.inject.Inject;
 
 @Extension(provides = I18nBundle.class, id = AdminMenuEntryResources.BUNDLENAME)
-public class AdminMenuEntryResources extends AbstractBundle
-{
-    
+public class AdminMenuEntryResources extends AbstractBundle {
     public static final String BUNDLENAME = AdminMenuEntry.PLUGIN_ID + ".AdminMenuEntryResources";
 
     @Inject
-    public AdminMenuEntryResources(BundleManager bundleManager)
-    {
+    public AdminMenuEntryResources(BundleManager bundleManager) {
         super(BUNDLENAME, bundleManager);
     }
 
-    public String getString(@PropertyKey(resourceBundle = BUNDLENAME) String key)
-    {
+    public String getString(@PropertyKey(resourceBundle = BUNDLENAME) String key) {
         return super.getString(key);
     }
 
-    public String format(@PropertyKey(resourceBundle = BUNDLENAME) String key, Object... obj)
-    {
+    public String format(@PropertyKey(resourceBundle = BUNDLENAME) String key, Object... obj) {
         return super.format(key, obj);
     }
 }
